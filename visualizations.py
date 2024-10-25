9470749863da6995c00f8d346f57106f67b85d1d
arnavmaniar <160043380+arnavmaniar@users.noreply.github.com>
2024-10-25 15:23:51 -0400

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,7 +1,7 @@
 #init 
 #run command python3 -m streamlit run visualizations.py
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
